6f6805d3a7402ec68f699441bacd14b05fa5d1dd
David Morgan <dmorgan81@gmail.com>
2018-08-28 09:28:15 -0400

    Background color for default layers

--- src/c/standard-types.c
+++ src/c/standard-types.c
@@ -4,6 +4,43 @@
 
 #define eq(s, t) (strncmp(s, t, sizeof(s)) == 0)
 
+struct DefaultLayerData {
+    GColor color;
+};
+
+static void prv_default_update_proc(Layer *layer, GContext *ctx) {
+    struct DefaultLayerData *data = layer_get_data(layer);
+    if (!gcolor_equal(data->color, GColorClear)) {
+        graphics_context_set_fill_color(ctx, data->color);
+        graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
+    }
+}
+
+static void *prv_default_layer_create(GRect frame) {
+    Layer *layer = layer_create_with_data(frame, sizeof(struct DefaultLayerData));
+    layer_set_update_proc(layer, prv_default_update_proc);
+    struct DefaultLayerData *data = layer_get_data(layer);
+    data->color = GColorClear;
+    return layer;
+}
+
+static void prv_default_layer_parse(Layout *layout, Json *json, void *object) {
+    Layer *layer = (Layer *) object;
+    struct DefaultLayerData *data = layer_get_data(layer);
+
+    size_t size = json_get_size(json);
+    for (size_t i = 0; i < size; i++) {
+        char key[16];
+        json_next_string(json, key, sizeof(key));
+        if (eq(key, "background")) {
+            data->color = json_next_color(json);
+            break;
+        } else {
+            json_skip_tree(json);
+        }
+    }
+}
+
 static void prv_text_layer_destroy(void *object) {
     TextLayer *layer = (TextLayer *) object;
     char *buf = (char *) text_layer_get_text(layer);
@@ -98,8 +135,9 @@ static void prv_bitmap_layer_destroy(void *object) {
 
 void add_standard_types(Layout *layout) {
     layout_add_type(layout, "Layer", (TypeFuncs) {
-        .create = (TypeCreateFunc) layer_create,
-        .destroy = (TypeDestroyFunc) layer_destroy
+        .create = prv_default_layer_create,
+        .destroy = (TypeDestroyFunc) layer_destroy,
+        .parse = prv_default_layer_parse
     }, NULL);
 
     layout_add_type(layout, "TextLayer", (TypeFuncs) {
